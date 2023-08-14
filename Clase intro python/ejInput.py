38a3d48868074374fed4995bc840557fd7daca86
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2023-08-14 13:43:50 -0600

    Intro a Python

---
+++ Clase intro python/ejInput.py
@@ -0,0 +1,8 @@
+mate = float(input("Dime la calificacion de mate: "))
+cienciasSociales = float(input("Dime ahora la calificacion de Ciencias Sociales: "))
+
+suma = mate + cienciasSociales
+print(suma)
+
+print(f"Sacaste {mate} en matematicas")
+print(f"Obtuviste {cienciasSociales} en la materia de Ciencias Sociales")
---
+++ Clase intro python/promedioUFs.py
@@ -0,0 +1,12 @@
+mate = 90
+cienciasSociales = 88
+filosofia = 70
+pensamiento = 83
+transformacion = 54
+desafios = 78
+progra = 100
+
+suma = mate + cienciasSociales + filosofia + pensamiento + transformacion + desafios + progra
+promedio = suma / 7
+
+print(f"El promedio del semestre es de {promedio} puntos")
